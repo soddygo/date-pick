0f8f23f72d9e84d99d99888e2a90c7f782edec66
宋国超 <soddygo@qq.com>
2018-12-04 14:49:10 +0800

    init save

--- src/purchase/main/main.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var handle;
-    define('purchase/main/main', function (require, exports, module) {
-        'kayak-version 201711061735';/*发布上线改版本号*/
-        module.exports = handle;
-    });
-})();
--- src/purchase/modules/menu/menu.tpl
+++
@@ -1,6 +0,0 @@
-<div class="purchase-menu">
-	<div class="sidebar-header clearfix" style="display: flex;">
-		<div class="logo"></div>
-	</div>
-	<div class="J_menuCont menu-content" id="J_menuCont"></div>
-</div>
